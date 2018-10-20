395455f225cf100bd3e1a837f910c97d226c1d58
Brandon Tucker <btucker84@gmail.com>
2018-10-20 00:27:54 -0400

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,11 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
   },
   extends: [
     'airbnb-base',
-    'plugin:vue/recommended'
-  ]
+    'plugin:vue/recommended',
+    'plugin:prettier/recommended',
+  ],
 };
