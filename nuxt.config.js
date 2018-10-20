8c48e9708d14d647824101a3aaa53842353be0b5
Brandon Tucker <btucker84@gmail.com>
2018-10-20 00:00:19 -0400

    Add bootstrap

---
+++ nuxt.config.js
@@ -0,0 +1,15 @@
+export default {
+  css: [
+    '@/styles/main.scss',
+  ],
+  head: {
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' },
+    ],
+    title: 'Albany RV Resort',
+  }
+}
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,21 @@
 <template>
   <h1>Hello world!</h1>
 </template>
+
+<script>
+if (process.browser) {
+  require('bootstrap');
+}
+
+export default {
+  head() {
+    return {
+      titleTemplate: '%s - Home',
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        { hid: 'description', name: 'description', content: 'My custom description' }
+      ]
+    };
+  }
+};
+</script>
