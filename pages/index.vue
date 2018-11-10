ab4b59c6de18e9f529251e8ef076e465cbe749b7
Brandon Tucker <btucker84@gmail.com>
2018-11-09 20:02:26 -0500

    Add hero with call to action

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,48 @@
 <template>
-  <div 
-    class="container mt-5" 
-    style="height: 2000px">
-    <p>This is the content for <code>index.vue</code>.</p>
-  </div>
+  <main
+    role="main">
+    <div style="background-image: url(https://source.unsplash.com/random)">
+      <div class="overlay">
+        <div class="container d-flex h-100">
+          <div class="m-auto">
+            <p class="display-4 text-white text-center mb-4">Reserve Your Site Today!</p>
+            <form>
+              <div class="form-row justify-content-center">
+                <div class="form-group col-md-3">
+                  <input
+                    type="text"
+                    class="form-control form-control-lg"
+                    placeholder="Check-In">
+                </div>
+                <div class="form-group col-md-3">
+                  <input
+                    type="text"
+                    class="form-control form-control-lg"
+                    placeholder="Check-Out">
+                </div>
+                <div class="form-group col-md-2">
+                  <select class="form-control form-control-lg">
+                    <option>Adults</option>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </select>
+                </div>
+                <div class="form-group">
+                  <a
+                    class="btn btn-lg btn-primary"
+                    style="margin-right: 5px; margin-left: 5px"
+                    href="#">Book Now</a>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script>
@@ -23,3 +62,17 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.overlay {
+  background: rgba(0, 0, 0, 0.5);
+  height: calc(100vh - 152px);
+  min-height: 515px;
+}
+
+@media screen and (min-width: 768px) {
+  .overlay {
+    height: calc(100vh - 102px);
+  }
+}
+</style>
