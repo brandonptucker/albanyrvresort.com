0c5f7d014557ba849979d86f3cf75766ed27a9e7
Brandon Tucker <btucker84@gmail.com>
2018-11-18 18:46:22 -0500

    Add rates page

---
+++ components/RateCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="col-md-4 mb-5">
+    <div
+      class="card"
+      style="box-shadow: 0 0.25rem 0.75rem rgba(0, 0, 0, .05)"
+    >
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal">{{ title }}</h4>
+      </div>
+      <div class="card-body">
+        <h1 class="display-4 mb-0 py-3">${{ rate }}</h1>
+        <router-link
+          class="btn btn-lg btn-block btn-primary mt-3"
+          to="reservations"
+        >
+          Book Now
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: { type: String, required: true },
+    rate: { type: Number, required: true },
+  },
+};
+</script>
---
+++ pages/rates.vue
@@ -0,0 +1,102 @@
+<template>
+  <main role="main">
+    <hero />
+    <div class="container mt-5 text-center">
+      <ul
+        id="pills-tab"
+        class="nav nav-pills justify-content-center mb-5"
+        role="tablist"
+      >
+        <li class="nav-item">
+          <a
+            id="pills-home-tab"
+            class="nav-link active show"
+            data-toggle="pill"
+            href="#pills-home"
+            role="tab"
+            aria-controls="pills-home"
+            aria-selected="true"
+          >
+            30 Amp
+          </a>
+        </li>
+        <li class="nav-item">
+          <a
+            id="pills-profile-tab"
+            class="nav-link"
+            data-toggle="pill"
+            href="#pills-profile"
+            role="tab"
+            aria-controls="pills-profile"
+            aria-selected="false"
+          >
+            50 Amp
+          </a>
+        </li>
+      </ul>
+      <div
+        id="pills-tabContent"
+        class="tab-content"
+      >
+        <div
+          id="pills-home"
+          class="tab-pane fade active show"
+          role="tabpanel"
+          aria-labelledby="pills-home-tab"
+        >
+          <div class="row">
+            <rate-card
+              :rate="32"
+              title="Daily"
+            />
+            <rate-card
+              :rate="175"
+              title="Weekly"
+            />
+            <rate-card
+              :rate="450"
+              title="Monthly"
+            />
+          </div>
+        </div>
+        <div
+          id="pills-profile"
+          class="tab-pane fade"
+          role="tabpanel"
+          aria-labelledby="pills-profile-tab"
+        >
+          <div class="row">
+            <rate-card
+              :rate="36"
+              title="Daily"
+            />
+            <rate-card
+              :rate="190"
+              title="Weekly"
+            />
+            <rate-card
+              :rate="500"
+              title="Monthly"
+            />
+          </div>
+        </div>
+      </div>
+      <p class="mb-5">
+        All rates include 30 or 50 amp electricity, water, sewer, cable TV,
+        level cement patio, picnic table, and free wireless internet.
+      </p>
+    </div>
+  </main>
+</template>
+
+<script>
+import Hero from '../components/Hero.vue';
+import RateCard from '../components/RateCard.vue';
+
+export default {
+  components: {
+    Hero,
+    RateCard,
+  },
+};
+</script>
