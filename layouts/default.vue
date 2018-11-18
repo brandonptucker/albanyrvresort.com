bf47b216841e0445e01e76d7eaec816d9f6d1042
Brandon Tucker <btucker84@gmail.com>
2018-11-18 16:19:01 -0500

    Add footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,12 +3,14 @@
     <AppHeader />
     <Navbar />
     <nuxt />
+    <AppFooter />
   </div>
 </template>
 
 <script>
 import AppHeader from '../components/AppHeader.vue';
 import Navbar from '../components/Navbar.vue';
+import AppFooter from '../components/AppFooter.vue';
 
 if (process.browser) {
   require('bootstrap'); // eslint-disable-line
@@ -18,6 +20,7 @@ export default {
   components: {
     AppHeader,
     Navbar,
+    AppFooter,
   },
 };
 </script>
