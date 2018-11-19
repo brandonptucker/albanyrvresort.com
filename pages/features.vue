8124658aac74b47272601ffcc0b782b7483e6e5c
Brandon Tucker <btucker84@gmail.com>
2018-11-18 19:28:17 -0500

    Add features page

---
+++ pages/features.vue
@@ -0,0 +1,88 @@
+<template>
+  <main role="main">
+    <hero />
+    <div class="container my-5">
+      <h4>Extra Large Pull Thru Sites</h4>
+      <p class="mb-5">
+        Our sites were designed specifically to accommodate the largest RV's in
+        production today. Each pull thru is 120 ft. long by 60 ft. wide. These
+        sites are large enough campers never have to unhook the RV or tow vehicle.
+      </p>
+      <h4>Large Level Concrete Patios</h4>
+      <p class="mb-5">
+        Each site has a level 40 ft. long by 20 ft. wide concrete patio and
+        picnic table.
+      </p>
+      <h4>Full Hookups</h4>
+      <p class="mb-5">
+        Every site features 20/30/50 AMP electric outlets, water, sewer, and
+        cable TV with 76+ channels.
+      </p>
+      <h4>Easily Accessible Sites</h4>
+      <p class="mb-5">
+        The roads and sites were designed to provide an easy entrance and exit
+        of the campsite.
+      </p>
+      <h4>Free Wifi Internet</h4>
+      <p class="mb-5">
+        Wireless internet is provided as bonus to our customers. Phone and
+        internet hookups are also available in the clubhouse.
+      </p>
+      <h4>Fishing</h4>
+      <p class="mb-5">
+        Our one acre pond is stocked with bream, bass, and channel catfish.
+      </p>
+      <h4>Clubhouse</h4>
+      <p class="mb-5">
+        Relax in our cozy 4100 sq. ft. clubhouse with elegant office, fitness
+        center, laundry room, bath houses, and a helpful, friendly staff.
+      </p>
+      <h4>Handicap Accessible Showers / Bathrooms</h4>
+      <p class="mb-5">
+        The bathrooms and showers feature ceramic tile and added safety features
+        for handicap needs. Management has placed a strong emphasis on
+        cleanliness, and will continue throughout the future.
+      </p>
+      <h4>Laundry Facility</h4>
+      <p class="mb-5">
+        Keep your clothes and linens clean without the hassle of ever leaving
+        the resort. Our strong emphasis on cleanliness extends to this area also.
+      </p>
+      <h4>Fitness Center</h4>
+      <p class="mb-5">
+        Stay fit while enjoying your stay with our large fitness room and great
+        selection of equipment.
+      </p>
+      <h4>Pet Walk</h4>
+      <p class="mb-5">
+        We love pets and have a very unique walking area that stretches nearly a
+        half mile in overall length.
+      </p>
+      <h4>Emergency Medical Station</h4>
+      <p class="mb-5">
+        EMS Station located less than 200 yards from the resort.
+      </p>
+      <h4>The Good Life City</h4>
+      <p class="mb-5">
+        Albany RV Resort is located near all of the dining, shopping, golfing,
+        and other attractions Albany, Georgia has to offer.
+      </p>
+      <h4>Team RV</h4>
+      <p class="mb-5">
+        <a href="http://teamrvinc.com">Team RV</a> is located directly next
+        door. They offer RV parts, sales, service, and a propane filling station.
+        On-site repair available with most repairs.
+      </p>
+    </div>
+  </main>
+</template>
+
+<script>
+import Hero from '../components/Hero.vue';
+
+export default {
+  components: {
+    Hero,
+  },
+};
+</script>
