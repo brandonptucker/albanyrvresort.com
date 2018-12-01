38dc4a888719cf6c57d5946a3a194331b4a6658d
Brandon Tucker <btucker84@gmail.com>
2018-12-01 13:26:46 -0500

    Update reservation page with state

--- pages/reservations.vue
+++ pages/reservations.vue
@@ -42,39 +42,47 @@
         <div class="form-row">
           <div class="col-md-3 form-group">
             <label>Check In</label>
-            <input
-              class="form-control"
-              type="date"
-              required
-            >
+            <datepicker
+              :disabled-dates="{ to: new Date() }"
+              v-model="checkin"
+              placeholder="Check-In"
+              input-class="form-control"
+              format="MMM dd yyyy"
+              @selected="checkinSelected" />
           </div>
           <div class="col-md-3 form-group">
             <label>Check Out</label>
-            <input
-              class="form-control"
-              type="date"
-              required
-            >
+            <datepicker
+              :disabled-dates="checkin ? { to: addDays(checkin, 1) } : { to: new Date() }"
+              v-model="checkout"
+              placeholder="Check-Out"
+              input-class="form-control"
+              format="MMM dd yyyy" />
           </div>
           <div class="col-md-3 form-group">
             <label>Adults</label>
-            <input
-              class="form-control"
-              type="number"
-              value="2"
-              min="1"
-              required
-            >
+            <select
+              v-model="adults"
+              class="form-control">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </select>
           </div>
           <div class="col-md-3 form-group">
             <label>Children</label>
-            <input
-              class="form-control"
-              type="number"
-              value="0"
-              min="0"
-              required
-            >
+            <select
+              v-model="children"
+              class="form-control">
+              <option>0</option>
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </select>
           </div>
         </div>
         <div class="form-row">
@@ -98,11 +106,58 @@
 </template>
 
 <script>
+import Datepicker from 'vuejs-datepicker';
 import Hero from '../components/Hero.vue';
 
 export default {
   components: {
+    Datepicker,
     Hero,
+  },
+  data() {
+    return {
+      children: 0,
+    };
+  },
+  computed: {
+    checkin: {
+      get() {
+        return this.$store.state.checkin;
+      },
+      set(value) {
+        this.$store.commit('updateCheckin', value);
+      },
+    },
+    checkout: {
+      get() {
+        return this.$store.state.checkout;
+      },
+      set(value) {
+        this.$store.commit('updateCheckout', value);
+      },
+    },
+    adults: {
+      get() {
+        return this.$store.state.adults;
+      },
+      set(value) {
+        this.$store.commit('updateAdults', value);
+      },
+    },
+  },
+  methods: {
+    checkinSelected(selected) {
+      if (selected >= this.checkout) {
+        const date = new Date(selected);
+        date.setDate(date.getDate() + 1);
+        this.checkout = date;
+      }
+    },
+    addDays(date, days) {
+      const d = new Date(date);
+      d.setDate(date.getDate() + days);
+      return d;
+    },
   },
 };
 </script>
