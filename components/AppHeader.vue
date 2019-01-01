f9d1f87420654f027a7cc4dee30615c2fd3c1c54
Brandon Tucker <btucker84@gmail.com>
2018-12-31 19:22:40 -0500

    Update social media links

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -3,15 +3,17 @@
     <div class="container d-flex flex-column flex-md-row pt-2">
       <a
         class="mr-3 mb-1"
-        href="#"><font-awesome-icon icon="phone" /> (229) 431-2229</a>
-      <a
-        class="mr-auto mb-1"
-        href="#"><font-awesome-icon icon="map-marker-alt" /> 1202 Liberty Expy SE Albany, GA 31705</a>
+        href="tel:1-229-431-2229"><font-awesome-icon icon="phone" /> (229) 431-2229</a>
+      <div class="mr-auto mb-1">
+        <font-awesome-icon icon="map-marker-alt" /> 1202 Liberty Expy SE Albany, GA 31705
+      </div>
       <ul class="list-inline mb-2">
         <li class="list-inline-item">
           <a
             class="facebook"
-            href="#"><font-awesome-icon :icon="['fab', 'facebook']" /> Facebook</a>
+            href="https://www.facebook.com/Albany-RV-Resort-Inc-111460132227079/"
+            target="_blank"
+          ><font-awesome-icon :icon="['fab', 'facebook']" /> Facebook</a>
         </li>
         <li class="list-inline-item">
           <a
@@ -21,7 +23,9 @@
         <li class="list-inline-item">
           <a
             class="twitter"
-            href="#"><font-awesome-icon :icon="['fab', 'twitter']" /> Twitter</a>
+            href="https://twitter.com/albanyrvresort"
+            target="_blank"
+          ><font-awesome-icon :icon="['fab', 'twitter']" /> Twitter</a>
         </li>
       </ul>
     </div>
@@ -32,6 +36,7 @@
 header {
   font-size: 0.875rem;
   font-weight: lighter;
+  color: rgba(255, 255, 255, 0.5);
 }
 
 header a {
